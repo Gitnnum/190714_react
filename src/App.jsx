ff8a267b7983fea3d92628a16cb2d1ab68303698
liule <liule@qq.com>
2019-07-14 16:27:38 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+import {Route,Switch} from 'react-router-dom'
+import {HashRouter,BrowserRouter} from 'react-router-dom'
+export default class App extends Component{
+    render(){
+        return (
+            <div> 
+                <h1>哈哈哈哈</h1>
+            </div>
+        )
+    }
+}
